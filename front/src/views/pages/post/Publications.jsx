be19d93441ad56d44b3c7545787f29a7c70020ab
SebastianC <sebas99cano@gmail.com>
2021-07-28 14:45:32 -0500

    se agrego el select, para filtrar por categorias con su respectivo useState

--- front/src/views/pages/post/Publications.jsx
+++ front/src/views/pages/post/Publications.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Publications = () => {
+    const [category, setCategory] = useState('all');
     return (
         <div className="container text-center">
             <div className="row">
-                <div className="col-md-3"/>
+                <div className="col-md-3">
+                    <div className="card my-5">
+                        <div className="card-body">
+                            <h3 className="card-title text-center">Select category</h3>
+                            <select className="custom-select"
+                                    value={category}
+                                    onChange={(event => setCategory(event.target.value))}>
+                                <option value="All">All</option>
+                                <option value="Action">Action</option>
+                                <option value="Anime">Anime</option>
+                                <option value="Suspense">Suspense</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
                 <div className="col-md-6">
                     <br/><br/>
                     <h1 className="cover-heading mt-5 font-weight-bold">
-                        Publications <i className="bi bi-camera-reels"/>
+                        {category} Publications <i className="bi bi-camera-reels"/>
                     </h1>
                 </div>
             </div>
