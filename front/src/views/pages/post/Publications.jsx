0648a44924e38b3faccc1fde05a8606c9f8fdbd3
SebastianC <sebas99cano@gmail.com>
2021-07-28 15:42:30 -0500

    prototipo de la pagina aplications.jsx

--- front/src/views/pages/post/Publications.jsx
+++ front/src/views/pages/post/Publications.jsx
@@ -3,9 +3,20 @@ import React, {useState} from "react";
 const Publications = () => {
     const [category, setCategory] = useState('all');
     return (
-        <div className="container text-center">
+        <div className="container">
             <div className="row">
-                <div className="col-md-3">
+                <div className="col-md-4">
+                    <div className="card my-5">
+                        <div className="card-body">
+                            <h3 className="card-title text-center">Instructions</h3>
+                            <p className={"card-text"}>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                Fugit iste nobis possimus? Ad culpa dignissimos distinctio
+                                fuga incidunt ipsum, iure magni molestias odio pariatur perferendis
+                                quo quos reprehenderit velit vitae?
+                            </p>
+                        </div>
+                    </div>
                     <div className="card my-5">
                         <div className="card-body">
                             <h3 className="card-title text-center">Select category</h3>
@@ -20,11 +31,37 @@ const Publications = () => {
                         </div>
                     </div>
                 </div>
-                <div className="col-md-6">
-                    <br/><br/>
-                    <h1 className="cover-heading mt-5 font-weight-bold">
+                <div className="col-md-8">
+                    <h1 className="my-5 text-center cover-heading mt-5 font-weight-bold">
                         {category} Publications <i className="bi bi-camera-reels"/>
                     </h1>
+                    <div className="card my-5">
+                        <div className="card-body">
+                            <h5 className="card-title">First Publication</h5>
+                            <h6 className="card-subtitle mb-2 ">Category: Action</h6>
+                            <p className={"card-text"}>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit iste nobis possimus? Ad culpa dignissimos distinctio fuga incidunt ipsum, iure magni molestias odio pariatur perferendis quo quos reprehenderit velit vitae?
+                            </p>
+                        </div>
+                    </div>
+                    <div className="card my-5">
+                        <div className="card-body">
+                            <h5 className="card-title">Second Publication</h5>
+                            <h6 className="card-subtitle mb-2 ">Category: Suspense</h6>
+                            <p className={"card-text"}>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit iste nobis possimus? Ad culpa dignissimos distinctio fuga incidunt ipsum, iure magni molestias odio pariatur perferendis quo quos reprehenderit velit vitae?
+                            </p>
+                        </div>
+                    </div>
+                    <div className="card my-5">
+                        <div className="card-body">
+                            <h5 className="card-title">Third Publication</h5>
+                            <h6 className="card-subtitle mb-2 ">Category: Anime</h6>
+                            <p className={"card-text"}>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit iste nobis possimus? Ad culpa dignissimos distinctio fuga incidunt ipsum, iure magni molestias odio pariatur perferendis quo quos reprehenderit velit vitae?
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
