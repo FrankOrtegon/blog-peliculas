30f22d803ffce240b5d615b1c80460aeca87db0c
SebastianC <sebas99cano@gmail.com>
2021-07-28 19:00:04 -0500

    se creo el endpoint para insertar usuarios

---
+++ back-springboot/infrastructure/entry-points/api-rest/src/main/java/co/com/sofka/api/UserDTO.java
@@ -0,0 +1,44 @@
+package co.com.sofka.api;
+
+import co.com.sofka.model.count.values.Email;
+import co.com.sofka.model.count.values.IdUser;
+import co.com.sofka.model.count.values.UrlPhoto;
+
+public class UserDTO {
+    private IdUser idUser;
+    private Email email;
+    private UrlPhoto urlPhoto;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(IdUser idUser, Email email, UrlPhoto urlPhoto) {
+        this.idUser = idUser;
+        this.email = email;
+        this.urlPhoto = urlPhoto;
+    }
+
+    public IdUser getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(IdUser idUser) {
+        this.idUser = idUser;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public void setEmail(Email email) {
+        this.email = email;
+    }
+
+    public UrlPhoto getUrlPhoto() {
+        return urlPhoto;
+    }
+
+    public void setUrlPhoto(UrlPhoto urlPhoto) {
+        this.urlPhoto = urlPhoto;
+    }
+}
