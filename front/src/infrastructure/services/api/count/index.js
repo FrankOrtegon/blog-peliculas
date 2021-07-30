3c2068cef25977a4db65330be66f0f76c930f8c7
SebastianC <sebas99cano@gmail.com>
2021-07-30 12:36:56 -0500

    implementacion de axios

--- front/src/infrastructure/services/api/count/index.js
+++ front/src/infrastructure/services/api/count/index.js
@@ -1,5 +1,14 @@
+import axios from "axios";
+
 const functions = {
     getCount: (userId) =>{
+        const idFake="intento3"
+        axios.get('https://blogmovie.herokuapp.com/api/get/count/'+idFake).then(response => {
+            console.log(response)
+            const count = response.data;
+            console.log(count)
+        })
+
         return {
             userid: userId,
             name:"Pancho",
@@ -8,6 +17,17 @@ const functions = {
         }
     },
     createCount: (userId) => {
+        const count = {
+            userid:userId,
+            name: "",
+            phone:"",
+            plan:false
+        }
+        axios.post('https://blogmovie.herokuapp.com/api/add/count',{count}).then(response => {
+            console.log(response)
+            const data = response.data;
+            console.log(data)
+        })
         return{
             userid:userId,
             name: "",
@@ -16,6 +36,12 @@ const functions = {
         }
     },
     updateCount: (count) => {
+
+        axios.put('https://blogmovie.herokuapp.com/api/update/count',{count}).then(response => {
+            console.log(response)
+            const data = response.data;
+            console.log(data)
+        })
         return{
             userid:count.userid,
             name: count.name,
