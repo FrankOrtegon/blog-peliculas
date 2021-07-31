b215c3342a31519ad9cbbdf45d58c18ee8125c6f
SebastianC <sebas99cano@gmail.com>
2021-07-30 21:53:48 -0500

    se agrega el endpoint nuevamente de publicaciones con el cors arreglado

--- front/src/infrastructure/services/api/publication/index.js
+++ front/src/infrastructure/services/api/publication/index.js
@@ -1,11 +1,11 @@
-//import axios from "axios";
+import axios from "axios";
 
 const functions = {
 
-    loadPublication: () => {
-        //const response =  await axios.get('https://nameless-mountain-68003.herokuapp.com/api/publication/')
-        //return response.data;
-        const response = [{
+    loadPublication: async () => {
+        const response2 =  await axios.get('https://nameless-mountain-68003.herokuapp.com/api/publication/')
+        console.log(response2.data)
+       const response = [{
             idCount:"TsOdQhKj0kNGF0aiNeJW4WxCGA22",
             id: "asdasda",
             name: "titulo",
--- front/src/views/pages/post/PublicationCreate.jsx
+++ front/src/views/pages/post/PublicationCreate.jsx
@@ -19,38 +19,36 @@ export const PublicationCreate = ({idCount}) => {
     }
 
     return (
-        <div className="col-md-6 mx-auto">
-            <div className="card">
-                <div className="card-header">
-                    <h3>Form to create Publication</h3>
-                </div>
-                <div className="card-footer">
-                    <label className={"m-3"}>Name</label>
-                    <input placeholder={"Name"}
-                           className={"form-control"}
-                           name={"name"}
-                    value={name}
-                    onChange={event => setName(event.target.value)}/>
-                    <label className={"m-3"}>Category</label>
-                    <select className="custom-select"
-                            value={category}
-                            onChange={(event => setCategory(event.target.value))}>
-                        <option value="All">All</option>
-                        <option value="Action">Action</option>
-                        <option value="Anime">Anime</option>
-                        <option value="Suspense">Suspense</option>
-                    </select>
-                    <label className={"m-3"}>Description</label>
-                    <input type={"text"}
-                           placeholder={"Description"}
-                           className={"form-control"}
-                           name={"description"}/>
-                    
-                    <div className="text-center">
-                        <button onClick={publicationCreateSubmit} className="btn btn-success px-5 mt-4 ">Add <i
-                            className="bi bi-plus-square"/>
-                        </button>
-                    </div>
+        <div className="card">
+            <div className="card-header">
+                <h3>Form to create Publication</h3>
+            </div>
+            <div className="card-footer">
+                <label className={"m-3"}>Name</label>
+                <input placeholder={"Name"}
+                       className={"form-control"}
+                       name={"name"}
+                       value={name}
+                       onChange={event => setName(event.target.value)}/>
+                <label className={"m-3"}>Category</label>
+                <select className="custom-select"
+                        value={category}
+                        onChange={(event => setCategory(event.target.value))}>
+                    <option value="All">All</option>
+                    <option value="Action">Action</option>
+                    <option value="Anime">Anime</option>
+                    <option value="Suspense">Suspense</option>
+                </select>
+                <label className={"m-3"}>Description</label>
+                <input type={"text"}
+                       placeholder={"Description"}
+                       className={"form-control"}
+                       name={"description"}/>
+
+                <div className="text-center">
+                    <button onClick={publicationCreateSubmit} className="btn btn-success px-5 mt-4 ">Add <i
+                        className="bi bi-plus-square"/>
+                    </button>
                 </div>
             </div>
         </div>
